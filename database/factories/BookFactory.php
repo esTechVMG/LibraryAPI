a93062de0bcf89f4370c6636874fa1c91f8efcc3
BuildTools <unconfigured@null.spigotmc.org>
2021-02-21 01:16:58 +0100

    Store  random words instead of titles

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Book::class, function (Faker $faker) {
     return [
-        'title' => $faker->title,
+        'title' => $faker->words(8,true),
         'description' => $faker->paragraph(1,true),
         'quantity' => $faker->randomNumber(9,false)
     ];
